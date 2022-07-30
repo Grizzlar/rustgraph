3f16f9a7f7c371666a7922d089e0a24c4b40f337
Grizzlar <grizzlar99@gmail.com>
2022-07-30 11:24:03 +0900

    add logic for sync & receive loops

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,6 @@
 pub struct Event {
     sender: u8,
+    hash: u32,
     sparent: u32,
     gparent: u32,
     ts: u32
@@ -14,30 +15,50 @@ fn to_u32(bytes: &[u8]) -> u32 {
 }
 
 impl Event {
-    pub fn new(packet: &[u8]) -> Event {
+    pub fn new(sender: u8, hash: u32, sparent: u32, gparent: u32, ts: u32) -> Event {
+        // TODO: Calculate hash instead of receiving it as input
         Event {
-            sender: packet[0],
-            sparent: to_u32(&packet[1..=4]),
-            gparent: to_u32(&packet[5..=8]),
-            ts: to_u32(&packet[9..=12])
+            sender,
+            hash,
+            sparent,
+            gparent,
+            ts
+        }
+    }
+
+    pub fn from_bytes(packet: &[u8]) -> Event {
+        Event {
+            sender: packet[0] - 64,
+            hash: to_u32(&packet[1..=4]),
+            sparent: to_u32(&packet[5..=8]),
+            gparent: to_u32(&packet[9..=12]),
+            ts: to_u32(&packet[13..=16])
         }
     }
 
     pub fn visualize(&self) {
         println!("Event {{
     sender: {}
+    hash: {}
     sparent: {}
     gparent: {}
     ts: {}
-}}", self.sender, self.sparent, self.gparent, self.ts);
+}}", self.sender, self.hash, self.sparent, self.gparent, self.ts);
     }
 
-    pub fn to_bytes(&self) -> [u8; 13] {
-        let mut bytes = [0; 13];
+    pub fn to_bytes(&self) -> [u8; 17] {
+        let mut bytes = [0; 17];
         bytes[0] = self.sender;
-        bytes[1..=4].copy_from_slice(&self.sparent.to_le_bytes());
-        bytes[5..=8].copy_from_slice(&self.gparent.to_le_bytes());
-        bytes[9..=12].copy_from_slice(&self.ts.to_le_bytes());
+        bytes[1..=4].copy_from_slice(&self.hash.to_le_bytes());
+        bytes[5..=8].copy_from_slice(&self.sparent.to_le_bytes());
+        bytes[9..=12].copy_from_slice(&self.gparent.to_le_bytes());
+        bytes[13..=16].copy_from_slice(&self.ts.to_le_bytes());
         bytes
     }
+
+    pub fn sender(&self) -> u8 { self.sender }
+    pub fn hash(&self) -> u32 { self.hash }
+    pub fn sparent(&self) -> u32 { self.sparent }
+    pub fn gparent(&self) -> u32 { self.gparent }
+    pub fn ts(&self) -> u32 { self.ts }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,60 +1,108 @@
 use std::io::{Read, Write};
+use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 use std::net::{TcpListener, TcpStream};
 use std::collections::HashMap;
+use std::cell::RefCell;
 use crate::NodePool;
 use crate::event::Event;
 
 pub struct Node<'a> {
     id: u8,
     thread: Option<JoinHandle<()>>,
-    events: Vec<Event>,
     rounds: u8,
     pool: &'a NodePool<'a>
 }
 
+pub struct NodeData {
+    id: u8,
+    rounds: u8,
+    peers: HashMap<u8, String>
+}
+
 impl <'a> Node<'a> {
     pub fn new(id: u8, rounds: u8, pool: &'a NodePool<'a>) -> Node<'a> {
-        let events = Vec::new();
-        Node { id, thread: None, events, rounds, pool }
+        Node { id, thread: None, rounds, pool }
     }
 
     pub fn start(&mut self) {
         let node_count = self.pool.nodes.borrow().len() as u8;
+
         let mut map = HashMap::new();
+        let mut graphs: HashMap<u8, RefCell<Vec<Event>>> = HashMap::new();
         for i in 0..node_count {
             if i != self.id {
                 map.insert(i, format!("127.0.0.1:212{}", i));
             }
+            graphs.insert(i, RefCell::new(Vec::new()));
         }
-        let id = self.id;
 
-        let thread = thread::spawn(move || loop {
-            let listener = TcpListener::bind(format!("127.0.0.1:212{}", id)).unwrap();
-            for stream in listener.incoming() {
-                let stream = stream.unwrap();
-                handle_connection(stream, &map);
-            }
-        });
+        let thread = spawn_tread(self.id, self.rounds, map, graphs);
         self.thread = Some(thread);
     }
 }
 
-fn handle_connection(mut stream: TcpStream, peers: &HashMap<u8, String>) {
-    stream.set_read_timeout(Some(Duration::new(2000, 0)));
-    let mut buf = [0; 20];
-    stream.read(&mut buf);
+fn now() -> u32 {
+    // TODO: RETURNS u64
+    SystemTime::now().elapsed().unwrap().as_secs() as u32
+}
 
-    if buf.starts_with(b"START") {
-        let event = Event::new(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
-        event.visualize();
+fn spawn_tread(id: u8, rounds: u8, peers: HashMap<u8, String>, graphs: HashMap<u8, RefCell<Vec<Event>>>) -> JoinHandle<()> {    
+    thread::spawn(move || {
+        let graphs = Arc::new(Mutex::new(graphs));
+        let data = Arc::new(NodeData {
+            id,
+            rounds,
+            peers
+        });
 
-        let mut send = TcpStream::connect(peers.values().next().unwrap()).unwrap();
-        send.write(&event.to_bytes());
-    } else {
-        let event = Event::new(&buf[..]);
-        event.visualize();
+        let mut guard = graphs.lock().unwrap();
+        guard.get(&data.id).unwrap().borrow_mut().push(
+            Event::new(data.id, 0, 0, 0, now())
+        );
+        drop(guard);
+
+        let recv = (Arc::clone(&graphs), Arc::clone(&data));
+        let send = (Arc::clone(&graphs), Arc::clone(&data));
+        let receiver = thread::spawn(move || send_sync(recv));
+        let sender = thread::spawn(move || recv_sync(send));
+        receiver.join();
+        sender.join();
+    })
+}
+
+fn recv_sync(params: (Arc<Mutex<HashMap<u8, RefCell<Vec<Event>>>>>, Arc<NodeData>)) {
+    let graph = params.0;
+    let data = params.1;
+
+    let listener = TcpListener::bind(format!("127.0.0.1:212{}", data.id)).unwrap();
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        stream.set_read_timeout(Some(Duration::new(2000, 0))).unwrap();
+        let mut buf = [0; 20];
+        if let Ok(_read) = stream.read(&mut buf) {
+            // TODO: Verify data
+            let event = Event::from_bytes(&buf[..]);
+            event.visualize();
+            
+            // TODO: HASH
+            let guard = graph.lock().unwrap();
+            let mut self_events = guard.get(&data.id).unwrap().borrow_mut();
+            if let Some(gossip_events) = guard.get(&event.sender()) {
+                let mut gossip_events = gossip_events.borrow_mut();
+                gossip_events.push(event);
+                let self_parent_hash = self_events.last().unwrap().hash();
+                self_events.push(
+                    Event::new(data.id, 0, self_parent_hash, gossip_events.last().unwrap().hash(), now())
+                );
+            }
+        }
     }
+}
 
+fn send_sync(params: (Arc<Mutex<HashMap<u8, RefCell<Vec<Event>>>>>, Arc<NodeData>)) {
+    loop {
+
+    }
 }
